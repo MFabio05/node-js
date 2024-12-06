270ba5f4675bc474efc6ca5665233ea91e5ee80f
Fabio Cuevas <fcuevas2505@gmail.com>
2024-12-06 17:44:06 -0600

    Add clase 4

---
+++ Cuarta clase/middlewares/cors.js
@@ -0,0 +1,15 @@
+import cors from 'cors'
+
+const ACCEPTED_ORIGINS = [
+  'http://localhost:1230',
+  'http://localhost:8080'
+]
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = { }) => cors({
+  origin: (origin, callback) => {
+    if (!origin || acceptedOrigins.includes(origin)) {
+      return callback(null, true)
+    }
+    return callback(new Error('Not allowed by CORS'))
+  }
+})
